0895274e1d9f839eda098e19602dbb9a8e15285c
paek1127 <paek1127@gmail.com>
2024-11-27 22:21:22 +0900

    1단계

---
+++ src/main/java/com/ll/App.java
@@ -0,0 +1,18 @@
+package com.ll;
+
+import java.util.Scanner;
+
+public class App {
+    private final Scanner scanner;
+
+    public App() {
+        this.scanner = new Scanner(System.in);
+    }
+
+    public void run() {
+        System.out.println("=== 회원 앱 ===");
+
+        System.out.print("명령) ");
+        String cmd = scanner.nextLine();
+    }
+}
